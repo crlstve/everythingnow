0bc820595b4145f7a84763b0193b1d2fa6a9e535
crlstve <ecarles10@gmail.com>
2024-05-01 01:33:59 +0200

    May 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './*.php',
-    './templates/*.php',
-    './templates/**/*.php',
-    './template-parts/*.php'
-    './template-parts/**/*.php',
-    './blocks/*.php',
-    './blocks/**/*.php',
-    './patterns/*.php',
-    './patterns/**/*.php',
-    './assets/js/*.js',
-    './assets/js/**/*.js',
-    './inc/*.php',
-    './inc/**/*.php',
+  mode: 'jit',
+  purge: {
+    content: [
+      './*.php',
+      './templates/*.php',
+      './templates/**/*.php',
+      './template-parts/**/*.php',
+      './template-parts/*.php',
+      './inc/**/*.php',
+      './inc/*.php',
+      './blocks/**/*.php',
+      './blocks/*.php',
+      './classes/**/*.php',
+      './classes/*.php',
     ],
+  },
+  darkMode: false, //you can set it to media(uses prefers-color-scheme) or class(better for toggling modes via a button)
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 }
