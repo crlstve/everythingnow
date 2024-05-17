3fbf20fde31078d1ee40bd27c058dc42c0bc0ae2
crlstve <ecarles10@gmail.com>
2024-05-17 15:49:21 +0200

    Test

--- blocks/test/test.php
+++ blocks/test/test.php
@@ -1,19 +1,20 @@
 <?php
 // Check rows exists.
-if( have_rows('test_1') ):
+//if( have_rows('test_1') ):
     // Loop through rows.
-    while( have_rows('test_1') ) : the_row();
+    //while( have_rows('test_1') ) : the_row();
         // Case: Paragraph layout.
-        if( get_row_layout() == 'bloc_1' ):
+       // if( get_row_layout() == 'bloc_1' ):
             // Load sub field value.
-            $title = get_sub_field('title_1');
-            $text = get_sub_field('text_1'); 
-            $img = get_sub_field('img_1');
-            $vid = get_sub_field('vid_1');
+            $test = get_field('test_1');
+            $title = $test['title_1'];
+            $text = $test['text_1']; 
+            $img = $test['img_1'];
+            $vid = $test['vid_1'];
 ?>
     <div class="wrap flex flex-col md:flex-row gap-3 my-16 md:my-24">
         <div class="w-full md:w-1/2">
-            <?php if($title): ?><h2 class="font-bold text-xl"><?= esc_html($title); ?></h2><?php endif; ?>
+            <?php if($title): ?><h2 class="font-bold text-3xl mb-8"><?= esc_html($title); ?></h2><?php endif; ?>
             <?php if($text): ?><p class="text-base"><?= esc_html($text); ?></p><?php endif; ?>
         </div>
         <?php if($img || $vid):?>
@@ -27,7 +28,7 @@ if( have_rows('test_1') ):
         <?php endif; ?>
     </div>
   <?php 
-        endif;
-   endwhile;
-endif;
+    //    endif;
+   //endwhile;
+//endif;
 ?>
